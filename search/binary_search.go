52908fe70a1b7b06c151ebc0c754e292b0de31aa
seaerchin <jer005@e.ntu.edu.sg>
2020-08-29 01:15:57 +0800

    added binary search impl

---
+++ search/binary_search.go
@@ -0,0 +1,24 @@
+package search
+
+// BinarySearch requires that the array is already sorted before hand
+// it uses a pivot (the middle element in the array)
+// binary search then splits the array into 2 portions and searches based on whether
+// the target is greater/lesser than the pivot
+// binary search is efficient and has time complexity of O(log n)
+func BinarySearch(arr []int, target int) (index int) { 
+	var low, high = 0, len(arr) - 1 
+	for (low <= high) { 
+		var mid = (low + high) / 2 
+		var pivot = arr[mid]
+		if pivot == target { 
+			return mid 
+		}
+		if target > pivot { 
+			low = mid + 1 
+		} else { 
+			high = mid - 1
+		}
+	}
+	return -1 
+}
+
