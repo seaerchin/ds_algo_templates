7cd516fde24b8889915c178b08b292564d640c60
seaerchin <jer005@e.ntu.edu.sg>
2020-11-16 19:58:32 +0800

    added docs for countingsort

--- sort/counting_sort.go
+++ sort/counting_sort.go
@@ -1,6 +1,7 @@
 package sort
 
 // CountingSort is a sorting algorithm that sorts integers; counting sort is NOT a comparison sort
+// CountingSort runs in O(n) time and is usable for either integers in range [0, largest] or objects that can be hashed to integers in this range
 func CountingSort(arr []int, largest int) []int {
 	// we need largest to know the size of our counting array
 	counts := make([]int, largest + 1)
@@ -13,6 +14,8 @@ func CountingSort(arr []int, largest int) []int {
 		counts[i] = counts[i - 1] + counts[i]
 	}
 	res := make([]int, len(arr))
+	// at this stage, we know how many elements are smaller than arr[j] through the prefix array 
+	// we have to account for the position so we subtract 1
 	for j:= 0; j < len(arr); j++ {
 		res[counts[arr[j]] - 1] = arr[j]
 		counts[arr[j]]--
--- sort/counting_sort_test.go
+++ sort/counting_sort_test.go
@@ -7,7 +7,7 @@ import (
 
 func TestCountingSort(t *testing.T) { 
 	for _, table := range testTable {
-		got := CountingSort(table.input, 10)
+		got := CountingSort(table.input, 10) // instead of 10, this should be the largest element in the array
 		for idx := range got {
 			if table.expected[idx] != got[idx] {
 				t.Errorf("CountingSort(%v), expected: %v, got: %v", table.input, table.expected, got)
